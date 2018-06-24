de3352b6a455de9606ac1af4f480e1f2923dd5a7
Zaynitov_Albert <zaynitov1@yandex.ru>
2018-06-24 18:46:42 +0300

    Initial commit

---
+++ app/src/main/java/ru/startandroid/lesson5/Fragment1.java
@@ -0,0 +1,34 @@
+package ru.startandroid.lesson5;
+
+import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Альберт on 24.06.2018.
+ */
+
+public class Fragment1 extends Fragment {
+
+/*    TextView yext;
+    BroadcastReceiver broadcastReceiver;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1,container, false);
+  return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        yext=(TextView) view.findViewById(R.id.textView);
+    }
+}*/
+}
---
+++ app/src/main/java/ru/startandroid/lesson5/Fragment2.java
@@ -0,0 +1,32 @@
+package ru.startandroid.lesson5;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Альберт on 24.06.2018.
+ */
+
+public class Fragment2 extends Fragment {
+  /*  Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.fragment2, null);
+        button = (Button) inflate.findViewById(R.id.button);
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+*/
+
+
+    }
+
